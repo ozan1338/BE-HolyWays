0452f645352db6aa306962cd762d015c361dc7af
ozan1338 <akhmadfauzan1338@gmail.com>
2022-02-02 16:31:55 +0700

    check issues

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -180,9 +180,9 @@ const getUserById = async(req,res,next) => {
             }
         })
 
-        if(users[0].profile.photoProfile){
-            users[0].profile.photoProfile = process.env.PATH_FILE + users[0].profile.photoProfile
-        }
+        //if(users[0].profile.photoProfile){
+        //    users[0].profile.photoProfile = process.env.PATH_FILE + users[0].profile.photoProfile
+        //}
 
         res.send({
             status: "success",
